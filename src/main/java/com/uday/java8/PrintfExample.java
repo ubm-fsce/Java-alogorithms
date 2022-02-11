e9c64185a680b5da58e9321e613240d60b1c81d0
Udaya Bhasker Mattupalli <mub.oct05@gmail.com>
2022-02-10 22:22:00 -0800

    Commit on Feb 10 2022

---
+++ src/main/java/com/uday/java8/PrintfExample.java
@@ -0,0 +1,25 @@
+package com.uday.java8;
+
+import java.util.Scanner;
+
+public class PrintfExample {
+    public static void main(String[] args) {
+      //  scanExample();
+        System.out.printf("Hello %s!%n", "Uday");
+
+    }
+
+    private static void scanExample() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            //Complete this line
+            System.out.format("%-15s%03d%n", s1, x);
+
+        }
+        System.out.println("================================");
+    }
+}
+
