394211a644964045e0d485ecf9d668214329f7ba
Udaya Bhasker Mattupalli <mub.oct05@gmail.com>
2022-03-12 12:23:56 -0800

    Mar 012 2022 | Commit 2

--- src/main/java/lc/day3/longestcommonprefix/LongestCommonPrefix.java
+++ src/main/java/lc/day3/longestcommonprefix/LongestCommonPrefix.java
@@ -5,7 +5,7 @@ public class LongestCommonPrefix {
     public static void main(String[] args) {
         LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
         String[] str = {"flower", "flow", "flight", "fight"};
-        System.out.println("longestCommonPrefix2() : " + longestCommonPrefix.longestCommonPrefix2(str));
+        System.out.println("longestCommonPrefixBS() : " + longestCommonPrefix.longestCommonPrefixBS(str));
     }
 
     private String longestCommonPrefix(String[] strs) {
@@ -32,6 +32,47 @@ public class LongestCommonPrefix {
             }
         }
         return toFind;
+    }
+
+    // Seems Most Efficient
+    public String longestCommonPrefixBS(String[] strs) {
+        int start = 0;
+        // First we find the length of the shortest string
+        int end = minString(strs) - 1;
+        String substr = "";
+        // then just simple binary search algorithm
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            boolean isPrefix = true;
+            String temp = strs[0].substring(0, mid + 1);
+            // iterate all strings
+            for (String s : strs) {
+                // if some string doens't have a prefix then isPrefix = false and break
+                if (!s.substring(0, mid + 1).equals(temp)) {
+                    isPrefix = false;
+                    break;
+                }
+            }
+            // if prefix is valid
+            if (isPrefix) {
+                // move to the right side in binary search
+                start = mid + 1;
+                // and save it
+                substr = temp;
+            } else {
+                // else to the left side
+                end = mid - 1;
+            }
+        }
+        return substr;
+    }
+
+    private int minString(String[] strs) {
+        int min = strs[0].length();
+        for (String s : strs) {
+            if (s.length() < min) min = s.length();
+        }
+        return min;
     }
 
 }
