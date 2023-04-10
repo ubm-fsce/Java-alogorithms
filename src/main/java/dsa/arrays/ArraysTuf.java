40511cf9385e0c75651cc24af7bdacb2ca77dcbe
Udaya Bhasker Mattupalli <ubm.fsce@gmail.com>
2023-04-10 01:25:07 -0700

    Commit 1

---
+++ src/main/java/dsa/arrays/ArraysTuf.java
@@ -0,0 +1,24 @@
+package dsa.arrays;
+
+import java.util.Arrays;
+
+public class ArraysTuf {
+    public static void main(String[] args) {
+        // Longest Element in Array
+        // Option 1
+        int[] arr = { 1, 3, 2, 5, 4 };
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length - 1]);
+        // Option 2
+        int max2 = Arrays.stream(arr).reduce(Integer.MIN_VALUE, (x, y) -> x < y ? y : x);
+        System.out.println(max2);
+        // Option 3
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = arr[i] < max ? max : arr[i];
+        }
+        System.out.println(max);
+
+    }
+
+}
